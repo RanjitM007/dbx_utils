3235a93c6c90b03fe0b74f92ec59b4e9222d0069
Ranjit Maity <74052241+RanjitM007@users.noreply.github.com>
2024-07-08 18:23:03 +0530

    project url update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbx_utils',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
@@ -14,6 +14,11 @@ setup(
     description='A utility package for managing Databricks folders and permissions',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
+    project_urls={
+        'Documentation': 'https://github.com/RanjitM007/dbx_utils/wiki',  # Replace with your documentation link if any
+        'Source': 'https://github.com/RanjitM007/dbx_utils',
+        'Tracker': 'https://github.com/RanjitM007/dbx_utils/issues',
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
